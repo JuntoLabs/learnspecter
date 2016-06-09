ca907f706ce837c5ff7457583bc8fcb71e9cbac8
Alex Gunnarson <alexandergunnarson@gmail.com>
2016-06-09 12:52:06 -0600

    Added lein-environ for dev/production purposes to make easier

--- project.clj
+++ project.clj
@@ -7,32 +7,35 @@
             [lein-essthree "0.2.1" ]
             [lein-ancient  "0.6.10"]]
   :dependencies
-    [[org.clojure/clojure                       "1.9.0-alpha5"    ]
-     [org.clojure/clojurescript                 "1.9.36"          ]
+    [[org.clojure/clojure                       "1.9.0-alpha5"]
+     [org.clojure/clojurescript                 "1.9.36"      ]
      ; ==== NAMESPACE ====
-     [org.clojure/tools.namespace               "0.2.11"          ]
-     ; ==== ASYNC ====
-     [org.clojure/core.async                    "0.2.374"         ]
-     [re-frame                                  "0.7.0"           ]
+     [org.clojure/tools.namespace               "0.2.11"      ]
+     [org.clojure/core.async                    "0.2.374"     ]
+     [re-frame                                  "0.7.0"       ]
      [reagent                                   "0.5.1"
-       :exclusions [org.json/json]                                ]
-     [com.taoensso/sente                        "1.8.1"           ]
-     [com.taoensso/timbre                       "4.3.1"           ]
+       :exclusions [org.json/json]                            ]
+     [com.taoensso/sente                        "1.8.1"       ]
+     [com.taoensso/timbre                       "4.3.1"       ]
+     [clj-http                                  "3.1.0"       ]
      ; ==== RESOURCES ====
-     [com.stuartsierra/component                "0.3.1"           ]
-     [compojure                                 "1.5.0"           ]
-     [org.immutant/web                          "2.1.4"           ]
-     ; ==== AUTH ====  
-     [ring/ring-defaults                        "0.2.0"           ]
-     [bk/ring-gzip                              "0.1.1"           ]]
+     [com.stuartsierra/component                "0.3.1"       ]
+     [environ                                   "1.0.3"       ]
+     ; ==== SERVER ====
+     [compojure                                 "1.5.0"       ]
+     [http-kit                                  "2.1.19"      ] 
+     [org.immutant/web                          "2.1.4"       ]
+     [ring/ring-defaults                        "0.2.0"       ]
+     ]
    :profiles
-   {:dev {:injections [] ; (clojure.main/repl :print clojure.pprint/pprint)
+   {:dev {:env            {:profile "dev"}
+          :injections     [] ; (clojure.main/repl :print clojure.pprint/pprint)
           :resource-paths ["dev-resources"]
           :dependencies   [[figwheel "0.5.3-2"]]
-          :plugins [[lein-cljsbuild "1.1.3"]
-                    [lein-figwheel  "0.5.3-2"
-                      :exclusions [org.clojure/clojure
-                                   org.clojure/core.async]]]}}
+          :plugins        [[lein-cljsbuild "1.1.3"]
+                           [lein-figwheel  "0.5.3-2"
+                             :exclusions [org.clojure/clojure
+                                          org.clojure/core.async]]]}}
   :aliases {"all" ["with-profile" "dev:dev,1.5:dev,1.7"]
             "deploy-dev"      ["do" "clean," "install"]
             "deploy-prod"     ["do" "clean," "install," "deploy" "clojars"]
